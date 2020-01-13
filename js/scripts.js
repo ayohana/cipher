960797b5adf15cbc437b72f209815a10ab1ec19d
Adela Darmansyah and Andriy Veremyeyev <andriy@>
2020-01-13 14:39:23 -0800

    add jQuery functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,42 +3,41 @@ var input = prompt("Please, enter some sentence");
 console.log (input)
 
 var modify = function(input){
-
   var first = input.charAt(0);
   var last = input.charAt(input.length-1);
   var cap = (first + last).toUpperCase();
   return cap;
-
 }
 
 var reverse = function(a){
-
   return (a.charAt(a.length-1) + a.charAt(0)).toUpperCase();
-
 }
 
 var call = function(input) {
   alert(modify(input));
   alert(reverse(input));
   alert(input + " " + modify(input));
-  
+  var result = input + " " + modify(input);
+  return result; 
 };
 
-call(input);
+var fourth = function(b) {
+  var length = b.length;
+  var half = length / 2;
+  var round = Math.round(half);
+  var letter = b.charAt(round - 1);
+  var result = letter + " " + b;
+  return result;
+}
 
+var response = fourth(call(input));
+alert (response)
 
-
-
-// $(document).ready(function() {
-//   $("h1").click(function() {
-//     alert("This is a header.");
-//   });
-
-//   $("p").click(function() {
-//     alert("This is a paragraph.");
-//   });
-
-//   $("img").click(function() {
-//     alert("This is an image.");
-//   });
-// });
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert(input);
+  });
+  $("h2").click(function() {
+    alert(response);
+  });
+});
