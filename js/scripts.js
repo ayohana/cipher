348730a798c32c93350e899fe2e376d902704582
Adela Darmansyah and Andriy Veremyeyev <adela@>
2020-01-13 13:20:05 -0800

    Create basic structure

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var input = prompt("Please, enter some sentence");
+
+console.log (input)
+
+
+
+
+// $(document).ready(function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   });
+
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+
+//   $("img").click(function() {
+//     alert("This is an image.");
+//   });
+// });
